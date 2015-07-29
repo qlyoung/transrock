40f0f61830d9b5cb044ea0b23edcd4577d96b3b1
Quentin Young <unboundedcurve@gmail.com>
2015-07-29 00:35:19 -0400

    AddRoutesActivity: removed block for location, fixed crash when selecting multiple agencies before bottom sheet shows, added up button to toolbar, made back button dismiss sheet if it's showing

--- app/src/main/java/us/v4lk/transrock/AddRoutesActivity.java
+++ app/src/main/java/us/v4lk/transrock/AddRoutesActivity.java
@@ -3,10 +3,11 @@ package us.v4lk.transrock;
 import android.accounts.NetworkErrorException;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.flipboard.bottomsheet.BottomSheetLayout;
@@ -22,12 +24,16 @@ import com.flipboard.bottomsheet.BottomSheetLayout;
 import org.json.JSONException;
 
 import java.net.SocketTimeoutException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+import us.v4lk.transrock.adapters.AgencyAdapter;
 import us.v4lk.transrock.adapters.RouteSwitchAdapter;
 import us.v4lk.transrock.transloc.Agency;
 import us.v4lk.transrock.transloc.Route;
 import us.v4lk.transrock.transloc.TransLocAPI;
-import us.v4lk.transrock.adapters.AgencyAdapter;
 import us.v4lk.transrock.util.LocationManager;
 import us.v4lk.transrock.util.Util;
 
@@ -37,9 +43,10 @@ import us.v4lk.transrock.util.Util;
  */
 public class AddRoutesActivity extends AppCompatActivity {
 
+    BottomSheetLayout root;
     ListView agencyList;
+    ProgressBar bodyProgressBar, toolbarProgressBar;
     LocationManager locationManager;
-    ProgressBar agencyLoadingSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,35 +55,98 @@ public class AddRoutesActivity extends AppCompatActivity {
         // set content
         setContentView(R.layout.activity_add_routes);
 
-        // set toolbar as action bar
+        // capture content
+        root = (BottomSheetLayout) findViewById(R.id.addroute_bottomsheetlayout);
+
+        // set toolbar as action bar, enable back button
         Toolbar toolbar = (Toolbar) findViewById(R.id.addroute_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // capture listview
+        // capture views
         agencyList = (ListView) findViewById(R.id.addroute_agency_list);
-
-        // capture progressbar
-        agencyLoadingSpinner = (ProgressBar) findViewById(R.id.addroute_progressbar);
+        bodyProgressBar = (ProgressBar) findViewById(R.id.addroute_body_progressbar);
+        toolbarProgressBar = (ProgressBar) findViewById(R.id.addroute_toolbar_progressbar);
 
         // set listener
         AdapterView.OnItemClickListener agencyClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Agency agency = (Agency) view.getTag();
-                showRouteBottomSheet(agency);
+                Agency a = (Agency) view.getTag();
+                showRouteBottomSheet(a);
             }
         };
         agencyList.setOnItemClickListener(agencyClickListener);
 
         // build location manager
         locationManager = new LocationManager(this);
+
     }
     @Override
     protected void onStart() {
-        if (Util.isConnected(this)) {
-            FetchAgencies fa = new FetchAgencies();
-            fa.execute();
-        }
+        AsyncTask<Void, Integer, Agency[]> fetchAgencies = new AsyncTask<Void, Integer, Agency[]>() {
+
+            int numActive, numLocal;
+
+            @Override
+            protected Agency[] doInBackground(Void... params) {
+                // get last location;
+                Location loc = locationManager.getLocation();
+
+                Agency[] active = null,
+                        local = null,
+                        all = null;
+                try {
+                    active = new Agency[0]; //TODO: load in active routes from persistence
+                    local = loc != null ? TransLocAPI.getAgencies(loc, 10000) : new Agency[0];
+                    all = TransLocAPI.getAgencies();
+                } catch (SocketTimeoutException e) {
+                    publishProgress(R.string.error_network_timeout);
+                    this.cancel(true);
+                }
+                catch (NetworkErrorException e) {
+                    publishProgress(R.string.error_network_unknown);
+                    this.cancel(true);
+                }
+                catch (JSONException e) {
+                    publishProgress(R.string.error_bad_parse);
+                    this.cancel(true);
+                }
+
+                // concatenate all agencies into single array
+                Agency[][] cumulative = new Agency[][] { active, local, all };
+                Agency[] sum = new Agency[active.length + local.length + all.length];
+                int i = 0;
+                for (Agency[] al : cumulative)
+                    for (Agency a : al)
+                        sum[i++] = a;
+
+                numActive = active.length;
+                numLocal = local.length;
+
+                return sum;
+            }
+            @Override
+            public void onPostExecute(Agency[] result) {
+                // populate agency list
+                AgencyAdapter adapter = new AgencyAdapter(
+                        AddRoutesActivity.this,
+                        R.layout.agency_list_item,
+                        result,
+                        numActive,
+                        numLocal);
+                agencyList.setAdapter(adapter);
+
+                bodyProgressBar.setVisibility(View.GONE);
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                showError(values[0]);
+            }
+        };
+
+        if (Util.isConnected(this)) fetchAgencies.execute();
         else showError(R.string.error_network_disconnected);
 
         super.onStart();
@@ -89,20 +159,44 @@ public class AddRoutesActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return false;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (root.isSheetShowing())
+            root.dismissSheet();
+        else
+            super.onBackPressed();
     }
 
     private void showRouteBottomSheet(Agency agency) {
         // this AsyncTask will fetch the routes and call the other overload when it finishes.
         AsyncTask<Integer, Integer, Route[]> fetchRoutes = new AsyncTask<Integer, Integer, Route[]>() {
+
+            @Override
+            protected void onPreExecute() {
+                // before doing anything, check if we're connected
+                if (!Util.isConnected(AddRoutesActivity.this)) {
+                    showPopupError(R.string.error_network_disconnected);
+                    this.cancel(true);
+                }
+                else
+                    toolbarProgressBar.setVisibility(View.VISIBLE);
+            }
             @Override
             protected Route[] doInBackground(Integer... params) {
                 int id = params[0];
 
                 Route[] routes = null;
-                try {
-                    routes = TransLocAPI.getRoutes(id);
-                }
+
+                try { routes = TransLocAPI.getRoutes(id); }
+
                 catch (SocketTimeoutException e) {
                     publishProgress(R.string.error_network_timeout);
                     this.cancel(true);
@@ -120,8 +214,7 @@ public class AddRoutesActivity extends AppCompatActivity {
             }
             @Override
             protected void onPostExecute(Route[] routes) {
-                Message message = new Message();
-                message.obj = routes;
+                toolbarProgressBar.setVisibility(View.INVISIBLE);
                 showRouteBottomSheet(routes);
             }
 
@@ -134,14 +227,19 @@ public class AddRoutesActivity extends AppCompatActivity {
             protected void onProgressUpdate(Integer... values) {
                 showPopupError(values[0]);
             }
+
+            @Override
+            protected void onCancelled(Route[] routes) {
+                toolbarProgressBar.setVisibility(View.INVISIBLE);
+            }
         };
         fetchRoutes.execute(agency.agency_id);
     }
     private void showRouteBottomSheet(Route[] routes) {
-        // capture root view
-        BottomSheetLayout root = (BottomSheetLayout) findViewById(R.id.addroute_bottomsheetlayout);
+        // if sheet is already showing, do nothing
+        if (root.isSheetShowing()) return;
 
-        // inflate bottom sheet
+        // inflate bottom sheet content view
         View bottomSheet = getLayoutInflater().inflate(R.layout.bottomsheet_list, root, false);
 
         // capture & set the title
@@ -156,12 +254,15 @@ public class AddRoutesActivity extends AppCompatActivity {
                 routes);
         routeList.setAdapter(adapter);
 
+        boolean l = root.isSheetShowing();
+
         // show bottom sheet
         root.showWithSheetView(bottomSheet);
     }
+
     private void showError(int errorStringResource) {
         // make sure spinner is hidden
-        agencyLoadingSpinner.setVisibility(View.GONE);
+        bodyProgressBar.setVisibility(View.GONE);
 
         // capture error message view
         TextView emv = (TextView) findViewById(R.id.addroute_error);
@@ -179,75 +280,5 @@ public class AddRoutesActivity extends AppCompatActivity {
            });
 
         dgb.show();
-    }
-
-    /**
-     * Wraps TransLoc call for agencies, loads in result to agency list when
-     * it returns
-     */
-    class FetchAgencies extends AsyncTask<Void, Integer, Agency[]> {
-
-        int numActive, numLocal;
-
-        @Override
-        protected Agency[] doInBackground(Void... params) {
-            // TODO: fix this. seriously.
-            // wait a few seconds for api to connect
-            try { Thread.sleep(2000); } catch (Exception e) {}
-            Location loc = locationManager.getLocation();
-
-            Agency[] active = null,
-                     local = null,
-                     all = null;
-            try {
-                active = new Agency[0]; //TODO: load in active routes from persistence
-                local = loc == null ? TransLocAPI.getAgencies(loc, 10000) : new Agency[0];
-                all = TransLocAPI.getAgencies();
-            } catch (SocketTimeoutException e) {
-                publishProgress(R.string.error_network_timeout);
-                this.cancel(true);
-            }
-            catch (NetworkErrorException e) {
-                publishProgress(R.string.error_network_unknown);
-                this.cancel(true);
-            }
-            catch (JSONException e) {
-                publishProgress(R.string.error_bad_parse);
-                this.cancel(true);
-            }
-
-            // concatenate all agencies into single array
-            Agency[][] cumulative = new Agency[][] { active, local, all };
-            Agency[] sum = new Agency[active.length + local.length + all.length];
-            int i = 0;
-            for (Agency[] al : cumulative)
-                for (Agency a : al)
-                    sum[i++] = a;
-
-            return sum;
-        }
-        @Override
-        public void onPostExecute(Agency[] result) {
-            // populate agency list
-            AgencyAdapter adapter = new AgencyAdapter(
-                    AddRoutesActivity.this,
-                    R.layout.agency_list_item,
-                    result,
-                    numActive,
-                    numLocal);
-            agencyList.setAdapter(adapter);
-
-            agencyLoadingSpinner.setVisibility(View.GONE);
-        }
-
-        /**
-         * Since we never actually need to update progress, i'm hijacking this
-         * method and using it to display any potential error messages.
-         * Pretend the method is called "displayError".
-         */
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            showError(values[0]);
-        }
     }
 }

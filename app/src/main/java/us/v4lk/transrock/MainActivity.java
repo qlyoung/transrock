02411144f1d92e09054067a87776c149abd08dd0
Quentin Young <unboundedcurve@gmail.com>
2015-11-08 00:05:26 -0500

    made a custom ViewPager and set up some stuff so that it doesn't intercept touch events when the map is in view

--- app/src/main/java/us/v4lk/transrock/MainActivity.java
+++ app/src/main/java/us/v4lk/transrock/MainActivity.java
@@ -16,8 +16,10 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnPageChange;
 import us.v4lk.transrock.fragments.MapFragment;
 import us.v4lk.transrock.fragments.RoutesFragment;
+import us.v4lk.transrock.util.SmartViewPager;
 
 /**
  * Main activity. Switches between content fragments using a nav drawer.
@@ -25,7 +27,16 @@ import us.v4lk.transrock.fragments.RoutesFragment;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.main_toolbar) Toolbar toolbar;
-    @Bind(R.id.fragment_pager) ViewPager pager;
+    @Bind(R.id.fragment_pager) SmartViewPager pager;
+    @OnPageChange(R.id.fragment_pager) void onPageSelected(int position) {
+        if (position == 0) {
+            // disallow swiping on map and set drawer's item selection to the correct value
+            pager.setAllowSwiping(false);
+            drawer.setSelection(position);
+        }
+        else
+            pager.setAllowSwiping(true);
+    }
 
     /** the nav drawer */
     Drawer drawer;
@@ -65,10 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         // build drawer
         drawer = buildDrawer(R.id.drawer_container);
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
+        // trigger some setup that gets called on item selection
+        drawer.setSelection(0);
     }
 
     private Drawer buildDrawer(int rootView) {
---
+++ app/src/main/java/us/v4lk/transrock/util/SmartViewPager.java
@@ -0,0 +1,36 @@
+package us.v4lk.transrock.util;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * ViewPager that allows swiping to be turned on and off.
+ */
+public class SmartViewPager extends ViewPager {
+
+    private boolean allowSwiping = true;
+
+    public SmartViewPager(Context context) {
+        super(context);
+    }
+
+    public SmartViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return allowSwiping && super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return allowSwiping && super.onTouchEvent(ev);
+    }
+
+    public void setAllowSwiping(boolean allow) {
+        this.allowSwiping = allow;
+    }
+}
